d23a5a012f90c8135d59e7bd97e0c488d7b0e7ca
thijsheijden <hi@thijsheijden.nl>
2021-12-01 10:29:16 +0100

    Day 1 first half

--- 1/main.hs
+++ 1/main.hs
@@ -1,3 +1,16 @@
 main :: IO()
 main = do
-  print "test"
+  -- Load input file
+  s <- readFile "input.txt"
+
+  -- Convert strings to ints  
+  let depthMeasurements = map (\x -> read x :: Int) (lines s)
+
+
+
+  print (f depthMeasurements 0)
+
+f :: [Int] -> Int -> Int
+f (x:xs:xss) increases  | x < xs = f (xs:xss) (increases + 1)
+                        | x > xs = f (xs:xss) increases
+f (x:[]) increases = increases
