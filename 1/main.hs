166ea7c31a2659937eec553a876e564990ed66e1
thijsheijden <hi@thijsheijden.nl>
2021-12-01 10:55:03 +0100

    Day 1 second half

--- 1/main.hs
+++ 1/main.hs
@@ -1,3 +1,7 @@
+import Control.Applicative
+import Data.Traversable (sequenceA)
+import Data.List (tails)
+
 main :: IO()
 main = do
   -- Load input file
@@ -6,11 +10,18 @@ main = do
   -- Convert strings to ints  
   let depthMeasurements = map (\x -> read x :: Int) (lines s)
 
-
-
+  -- First half of puzzle
   print (f depthMeasurements 0)
+
+  -- Second half of puzzle
+  print (f2 depthMeasurements)
 
 f :: [Int] -> Int -> Int
 f (x:xs:xss) increases  | x < xs = f (xs:xss) (increases + 1)
-                        | x > xs = f (xs:xss) increases
+                        | otherwise = f (xs:xss) increases
 f (x:[]) increases = increases
+
+f2 :: [Int] -> Int
+f2 m = f (map sum $ window m) 0
+  where
+    window = foldr (zipWith (:)) (repeat []) . take 3 . tails
