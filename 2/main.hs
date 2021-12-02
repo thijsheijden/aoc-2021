36ee1f7c304aa0d9724fc074c3c462de3e0ae537
thijsheijden <hi@thijsheijden.nl>
2021-12-02 10:32:29 +0100

    Second half day 2

--- 2/main.hs
+++ 2/main.hs
@@ -6,7 +6,10 @@ main = do
   
   let x = f (map words $ lines input) 0 0
 
+  let x2 = f2 (map words $ lines input) 0 0 0
+
   print x
+  print x2
 
 f :: [[String]] -> Int -> Int -> Int
 f (x:xs) h v  | "forward" `isPrefixOf` head x = f xs (h + read (last x)  :: Int) v
@@ -14,3 +17,10 @@ f (x:xs) h v  | "forward" `isPrefixOf` head x = f xs (h + read (last x)  :: Int)
               | "up" `isPrefixOf` head x = f xs h (v - read (last x) :: Int)
               | "down" `isPrefixOf` head x = f xs h (v + read (last x) :: Int)
 f [] h v = h * v
+
+f2 :: [[String]] -> Int -> Int -> Int -> Int
+f2 (x:xs) h v aim   | "forward" `isPrefixOf` head x = f2 xs (h + read (last x)  :: Int) (v + (aim * read (last x) :: Int)) aim
+                    | "backward" `isPrefixOf` head x = f2 xs (h + read (last x) :: Int) v aim
+                    | "up" `isPrefixOf` head x = f2 xs h v (aim - read (last x) :: Int) 
+                    | "down" `isPrefixOf` head x = f2 xs h v (aim + read (last x) :: Int)
+f2 [] h v aim = h * v
