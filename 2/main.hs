12d53cec6fbdeadd396e85038368e9ed772f374c
thijsheijden <hi@thijsheijden.nl>
2021-12-02 11:00:30 +0100

    Day 2 cleanup

--- 2/main.hs
+++ 2/main.hs
@@ -4,23 +4,31 @@ main :: IO()
 main = do
   input <- readFile "input.txt"
   
-  let x = f (map words $ lines input) 0 0
+  -- First half
+  let x = mul $ foldl (\t v -> f (head v) (read $ last v :: Int) t) (0, 0) (map words $ lines input)
 
-  let x2 = f2 (map words $ lines input) 0 0 0
+  -- Second half
+  let x2 = mul2 $ foldl (\t v -> f2 (head v) (read $ last v :: Int) t) (0, 0, 0) (map words $ lines input)
 
   print x
   print x2
 
-f :: [[String]] -> Int -> Int -> Int
-f (x:xs) h v  | "forward" `isPrefixOf` head x = f xs (h + read (last x)  :: Int) v
-              | "backward" `isPrefixOf` head x = f xs (h + read (last x) :: Int) v
-              | "up" `isPrefixOf` head x = f xs h (v - read (last x) :: Int)
-              | "down" `isPrefixOf` head x = f xs h (v + read (last x) :: Int)
-f [] h v = h * v
+-- First half
+f :: String -> Int -> (Int, Int) -> (Int, Int)
+f "forward"   d (h, v) = (h + d, v)
+f "backward"  d (h, v) = (h - d, v)
+f "up"        d (h, v) = (h, v - d)
+f "down"      d (h, v) = (h, v + d)
 
-f2 :: [[String]] -> Int -> Int -> Int -> Int
-f2 (x:xs) h v aim   | "forward" `isPrefixOf` head x = f2 xs (h + read (last x)  :: Int) (v + (aim * read (last x) :: Int)) aim
-                    | "backward" `isPrefixOf` head x = f2 xs (h + read (last x) :: Int) v aim
-                    | "up" `isPrefixOf` head x = f2 xs h v (aim - read (last x) :: Int) 
-                    | "down" `isPrefixOf` head x = f2 xs h v (aim + read (last x) :: Int)
-f2 [] h v aim = h * v
+-- Second half
+f2 :: String -> Int -> (Int, Int, Int) -> (Int, Int, Int)
+f2 "forward"  d (h, v, aim) = (h + d, v + aim * d, aim)
+f2 "backward" d (h, v, aim) = (h - d, v, aim)
+f2 "up"       d (h, v, aim) = (h, v, aim - d)
+f2 "down"     d (h, v, aim) = (h, v, aim + d)
+
+mul2 :: (Int, Int, Int) -> Int
+mul2 (h, v, _) = h * v
+
+mul :: (Int, Int) -> Int
+mul (h, v) = h * v
